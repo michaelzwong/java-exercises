4bf93c41e464bb6dc8985942a8b2ee2ef2fbcf34
Michael Wong <michaelzhwong@gmail.com>
2018-09-25 18:06:18 -0400

    Renamed

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -4,7 +4,7 @@
  * @author michaelwong
  *
  */
-public class RomantoInteger {
+public class RomanToInteger {
 	/**
 	 * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
 	 * 
