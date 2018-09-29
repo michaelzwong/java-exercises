a4daa634ae44b67d7fbdb7f52d6090db61fb57de
Michael Wong <michaelzhwong@gmail.com>
2018-09-28 23:08:44 -0400

    Added a more efficient solution

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -37,4 +37,37 @@ public class LongestCommonPrefix {
         }
         return strs[0].substring(0,minLength);
     }
+	
+	/**
+	 * 
+	 * Finds the longest common prefix string amongst an array of strings.
+	 * If there is no common prefix, return an empty string "".
+	 * 
+	 * This is a more efficient solution.
+	 * 
+	 * @param strs
+	 * @param efficient
+	 * @return
+	 */
+	public String findLongestCommonPrefix(String[] strs, boolean efficient) {
+        int arrSize = strs.length; // Length of array
+        if(strs.length == 0) return "";
+        String minString = "";
+        int minLength = strs[0].length(); // Shortest string in strs
+        for(int i = 0; i < arrSize; i++) { // Find the shortest string
+            if(strs[i].length() <= minLength) {
+                minLength = strs[i].length();
+                minString = strs[i];
+            }
+        }
+        for(int i = 0; i < arrSize; i++) {
+        	// If the string does not contain the minimum string
+        	while(!(strs[i].substring(0, minString.length()).equals(minString))) {
+                System.out.println(minString);
+        		minString = minString.substring(0, minString.length()-1);
+        	}
+        	
+        }
+		return minString;
+	}
 }
