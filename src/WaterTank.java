ce10175f3374cd070e47b18846f50e73d002dabd
Michael Wong <michaelzhwong@gmail.com>
2018-10-09 19:14:52 -0400

    O(n) solution

--- src/WaterTank.java
+++ src/WaterTank.java
@@ -28,4 +28,26 @@ public class WaterTank {
         }
         return maxArea;
     }
+	
+	public int maxArea(int[] height, boolean simple) {
+        if(height.length < 2) return 0;
+        
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while(left < right) {
+        	int minHeight = height[left] < height[right] ? height[left]:height[right];
+        	int curArea = minHeight * (right-left);
+        	if(curArea > maxArea) maxArea = curArea;
+        	if(height[left] < height[right]) {
+        		left++;
+        	} else {
+        		right--;
+        	}
+        }
+		
+		return maxArea;
+		
+	}
 }
