396844d657189d1ec2ba0cd82e17c7badceed600
Michael Wong <michaelzhwong@gmail.com>
2018-11-13 19:51:12 -0500

    Comments

--- src/ArrayMax.java
+++ src/ArrayMax.java
@@ -19,28 +19,27 @@ public class ArrayMax {
             return 0;
         }
         
-        // keeps track of the global maximum sum for a subarray in nums
-        // intially, set it as the first element
-        int maxSoFar = nums[0];
+        // keeps track of the global maximum sum
+        int maxSumSoFar = nums[0];
         
         // records the max sum up to the current index i 
-        int maxEndingHere = 0;
+        int maxSumAtCurIndex = 0;
         
         // loop through elements in the array
         for(int i = 0; i < nums.length; i++) {
             // keep track of the sum of a subarray that is non-negative
-            maxEndingHere += nums[i];
+        	maxSumAtCurIndex += nums[i];
             
             // if the sum of a subarray is greater than the current sum, record a new max
-            if(maxSoFar < maxEndingHere) {
-                maxSoFar = maxEndingHere;
+            if(maxSumAtCurIndex > maxSumSoFar) {
+            	maxSumSoFar = maxSumAtCurIndex;
             }
             
-            // if the result is negative, we reset the subarray at sum 0
-            if(maxEndingHere < 0) {
-                maxEndingHere = 0;
+            // if the result is negative, we reset the subarray to have sum 0
+            if(maxSumAtCurIndex < 0) {
+            	maxSumAtCurIndex = 0;
             }
         }
-        return maxSoFar;
+        return maxSumSoFar;
     }
 }
