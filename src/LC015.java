9429733b5b4d68333369e66a87cc05558785c6ce
Michael Wong <michaelzhwong@gmail.com>
2018-12-28 14:21:45 -0800

    Edits

---
+++ src/LC015.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+/**
+ * LeetCode Medium
+ * 
+ * @author michaelwong
+ *
+ */
+public class LC015 {
+	/**
+	 * Given an array nums of n integers, are there elements a, b, c in nums such
+	 * that a + b + c = 0? Find all unique triplets in the array which gives the sum
+	 * of zero.
+	 * 
+	 * @param nums - the array of n integers
+	 * @return - Triplets that add up to 0
+	 */
+	public List<List<Integer>> threeSum(int[] nums) {
+
+		Arrays.sort(nums);
+		// [-1, 0, 1, 2,-1,-4] =>
+		// [-4,-1,-1, 0, 1, 2]
+
+		return null;
+	}
+}
