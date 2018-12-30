a6b8fd756dbced75b9967327c2412898d4f4ebfc
Michael Wong <michaelzhwong@gmail.com>
2018-12-29 17:38:48 -0800

    Three sum

--- src/LC015.java
+++ src/LC015.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * LeetCode Medium
@@ -16,12 +19,55 @@ public class LC015 {
 	 * @param nums - the array of n integers
 	 * @return - Triplets that add up to 0
 	 */
-	public List<List<Integer>> threeSum(int[] nums) {
-
-		Arrays.sort(nums);
-		// [-1, 0, 1, 2,-1,-4] =>
-		// [-4,-1,-1, 0, 1, 2]
-
-		return null;
-	}
+public List<List<Integer>> threeSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        // ex. [-1, 0, 1, 2,-1,-4] =>
+        //     [-4,-1,-1, 0, 1, 2]
+        
+        // ex. [ 3, 0,-2,-1, 1, 2] =>
+        //     [-2,-1, 0, 1, 2, 3]
+        
+        // ex. [-1, 0, 1, 2,-1,-4] =>
+        //     [-4,-1,-1, 0, 1, 2]
+        
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
+        
+        // Loop throught the array 
+        for(int i = 0; i < nums.length - 2; i++) {
+            
+            // First number of the triplet
+            int firstNum = nums[i];
+            
+            // Second and third numbers
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            // Find nums[left] and nums[right] that add up to -firstNum
+            // (Implies that the 3 numbers add up to 0)
+            while(left < right) {
+                if(nums[left] + nums[right] == -firstNum) {
+                    List<Integer> triplet = new ArrayList<Integer>();
+                    triplet.add(firstNum);
+                    triplet.add(nums[left]);
+                    triplet.add(nums[right]);
+                    // If triplet is unique
+                    if(!set.contains(triplet)) {
+                        list.add(triplet);
+                        set.add(triplet);
+                    }
+                    left++;
+                    right--;
+                } else if(nums[left] + nums[right] > -firstNum){
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        
+        return list;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Test classes and methods from here.
@@ -12,9 +15,31 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] nums = new int[] { 3, 3, 4, 5, 6, 1, 3, 8, 9 };
+//		int[] nums = new int[] { 3, 3, 4, 5, 6, 1, 3, 8, 9 };
+//
+//		System.out.println(SegregateEvenOdd.segregate(nums));
+		
+		 Set<ArrayList<Integer>> hs = new HashSet<ArrayList<Integer>>();
 
-		System.out.println(SegregateEvenOdd.segregate(nums));
+		    ArrayList<Integer> arr = new ArrayList<Integer>();
+
+		    arr.add(1);
+		    arr.add(2);
+		    arr.add(3);
+		    arr.add(4);
+
+		    hs.add(arr);
+
+		    ArrayList<Integer> arr1 = new ArrayList<Integer>();
+
+		    arr1.add(1);
+		    arr1.add(2);
+		    arr1.add(3);
+		    arr1.add(4);
+
+		    hs.add(arr1);
+
+		    System.out.println(hs.size());
 
 	}
 }
