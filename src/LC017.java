f3f769e56fb9c3fcbd504f114b1c8c951c85b826
Michael Wong <michaelzhwong@gmail.com>
2019-01-08 15:54:38 -0500

    Initial commit

---
+++ src/LC017.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+/**
+ * LeetCode Medium.
+ * 
+ * @author michaelwong
+ *
+ */
+public class LC017 {
+	/**
+	 * Given a string containing digits from 2-9 inclusive, return all possible
+	 * letter combinations that the number could represent.
+	 * 
+	 * A mapping of digit to letters (just like on the telephone buttons) is given
+	 * below. Note that 1 does not map to any letters.
+	 * 
+	 * @param digits
+	 * @return
+	 */
+	public List<String> letterCombinations(String digits) {
+		return null;
+	}
+}
