fd5aa006f9caab9e4b80a07b36a070c819aea041
Michael Wong <michaelzhwong@gmail.com>
2019-01-08 12:20:59 -0500

    Three sum closest

--- src/LC016.java
+++ src/LC016.java
@@ -12,17 +12,45 @@ public class LC016 {
 	 * in nums such that the sum is closest to target. Return the sum of the three
 	 * integers. You may assume that each input would have exactly one solution.
 	 *
-	 * @param nums - List of numbers
+	 * @param nums   - List of numbers
 	 * @param target - The numbers returned will have a sum closest to this target
 	 * @return int - The three numbers whose sum is closest to the target
 	 */
 	public int threeSumClosest(int[] nums, int target) {
-		
+
 		// Sorting will simplify the algorithm
 		Arrays.sort(nums);
-		
-		
-		return 0;
+
+		int minDiff = Integer.MAX_VALUE;
+		int minSum = Integer.MAX_VALUE;
+
+		// Loop through the array nums.length times
+		for (int i = 0; i < nums.length; i++) {
+			int cur = nums[i];
+			
+			int l = i + 1;
+			int r = nums.length - 1;
+
+			while(l < r) {
+				int left = nums[l];
+				int right = nums[r];
+				int sum = cur + left + right;
+				int diff = Math.abs(target - sum);
+
+				if (diff < minDiff) {
+					minDiff = diff;
+					minSum = sum;
+				}
+				if(sum > target) {
+					r--;
+				} else {
+					l++;
+				}
+			}
+
+		}
+
+		return minSum;
 	}
 
 }
