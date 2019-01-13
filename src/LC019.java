5de26fa16d29c01768ba94b52f0387441fa999bb
Michael Wong <michaelzhwong@gmail.com>
2019-01-13 15:42:04 -0500

    Initial commit

---
+++ src/LC019.java
@@ -0,0 +1,36 @@
+import LC002.ListNode;
+
+/**
+ * LeetCode Medium.
+ * 
+ * @author michaelwong
+ *
+ */
+public class LC019 {
+	/**
+	 * Given a linked list, remove the n-th node from the end of list and return its
+	 * head.
+	 * 
+	 * @param head
+	 * @param n
+	 * @return
+	 */
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		return null;
+	}
+
+	/**
+	 * Definition for singly-linked list.
+	 * 
+	 * @author michaelwong
+	 *
+	 */
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+}
