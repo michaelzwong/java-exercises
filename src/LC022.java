83799d333e96520697f840c6b4192b1301236ce6
Michael Wong <michaelzhwong@gmail.com>
2019-01-15 15:24:09 -0500

    Initial commit

--- src/LC022.java
+++ src/LC022.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * LeetCode Medium.
  * 
@@ -5,5 +7,14 @@
  *
  */
 public class LC022 {
-	
+	/**
+	 * Given n pairs of parentheses, write a function to generate all combinations
+	 * of well-formed parentheses.
+	 * 
+	 * @param n - The number of pairs of parentheses
+	 * @return List<String> - List of all the combinations.
+	 */
+	public List<String> generateParenthesis(int n) {
+		return null;
+	}
 }
