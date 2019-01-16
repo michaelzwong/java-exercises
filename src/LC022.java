bfdd400d8be0a3244c3f808b88c73e2264e88c86
Michael Wong <michaelzhwong@gmail.com>
2019-01-15 20:22:13 -0500

    Incomplete solution

--- src/LC022.java
+++ src/LC022.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * LeetCode Medium.
@@ -15,6 +18,47 @@ public class LC022 {
 	 * @return List<String> - List of all the combinations.
 	 */
 	public List<String> generateParenthesis(int n) {
-		return null;
+		
+		Queue<String> list = new LinkedList<String>();
+		
+		recurse(list, 0, n);
+		
+		return new ArrayList<String>(list);
+	}
+	
+	private void recurse(Queue<String> list, int curDepth, int depth) {
+		
+		// Base case
+		if(curDepth == 0) {
+			list.offer("");
+			recurse(list, ++curDepth, depth);
+		}
+		else if(curDepth <= depth) {
+			
+			int size = list.size();
+						
+			int i = 0;
+			
+			while(i < size) {
+				String s = list.poll();
+				
+				String a = "()" + s;
+				String b = "(" + ")";
+				
+				list.offer(a);
+				list.offer(b);
+				
+				if(i > 0) {
+					String c = s + "()";
+					list.offer(c);
+				}
+                i++;
+			}
+			
+			recurse(list, ++curDepth, depth);
+			
+		}
+        				
+		return;
 	}
 }
