ebf7af961c749107d4660b66c97acb11095b55d4
Michael Wong <michaelzhwong@gmail.com>
2019-01-20 22:51:27 -0500

    Initial commit

---
+++ src/LC023.java
@@ -0,0 +1,37 @@
+import LC021.ListNode;
+
+/**
+ * LeetCode Hard.
+ * 
+ * @author michaelwong
+ *
+ */
+public class LC023 {
+
+	/**
+	 * Merge k sorted linked lists and return it as one sorted list. Analyze and
+	 * describe its complexity.
+	 * 
+	 * @param lists - The list of linked lists to be merged.
+	 * @return ListNode - The head of the merged linked list.
+	 */
+	public ListNode mergeKLists(ListNode[] lists) {
+		return null;
+	}
+
+	/**
+	 * Definition for singly-linked list.
+	 * 
+	 * @author michaelwong
+	 *
+	 */
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+}
