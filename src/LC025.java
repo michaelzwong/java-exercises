9ecca326d4f1b802ff32f49f3621d91db7062bc1
Michael Wong <michaelzhwong@gmail.com>
2019-01-22 14:07:26 -0500

    Initial code

---
+++ src/LC025.java
@@ -0,0 +1,39 @@
+/**
+ * LeetCode Hard.
+ * 
+ * @author michaelwong
+ *
+ */
+public class LC025 {
+
+	/**
+	 * Given a linked list, reverse the nodes of a linked list k at a time and
+	 * return its modified list.
+	 * 
+	 * k is a positive integer and is less than or equal to the length of the linked
+	 * list. If the number of nodes is not a multiple of k then left-out nodes in
+	 * the end should remain as it is.
+	 * 
+	 * @param head - Head of linked list.
+	 * @param k - Reverse the linked list k at a time.
+	 * @return ListNode - Head of altered linked list.
+	 */
+	public ListNode reverseKGroup(ListNode head, int k) {
+		return null;
+	}
+
+	/**
+	 * Definition for singly-linked list.
+	 * 
+	 * @author michaelwong
+	 *
+	 */
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+}
