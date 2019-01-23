f3b88ae093b285a6013cfba6b7af24a233862f79
Michael Wong <michaelzhwong@gmail.com>
2019-01-23 01:19:18 -0500

    Recursive solution

--- src/LC025.java
+++ src/LC025.java
@@ -14,12 +14,59 @@ public class LC025 {
 	 * list. If the number of nodes is not a multiple of k then left-out nodes in
 	 * the end should remain as it is.
 	 * 
+	 * Only constant extra memory is allowed. You may not alter the values in the
+	 * list's nodes, only nodes itself may be changed.
+	 * 
 	 * @param head - Head of linked list.
-	 * @param k - Reverse the linked list k at a time.
+	 * @param k    - Reverse the linked list k at a time.
 	 * @return ListNode - Head of altered linked list.
 	 */
 	public ListNode reverseKGroup(ListNode head, int k) {
-		return null;
+		int count = getCount(head, k);
+
+		int numIters = count / k;
+
+		return reverseGroup(head, k, numIters);
+	}
+
+	private ListNode reverseGroup(ListNode node, int k, int numIters) {
+		if (numIters == 0) {
+			return node;
+		}
+		/* First node in the group */
+		ListNode tail = null;
+		/* Last node in the group */
+		ListNode head = null;
+
+		ListNode prev = null;
+		ListNode cur = node;
+		ListNode next = null;
+
+		// Loop through the linked list k times
+		for (int i = 0; i < k; i++) {
+			next = cur.next;
+			if (tail == null) {
+				tail = cur;
+			} else {
+				cur.next = prev;
+			}
+			prev = cur;
+			cur = next;
+		}
+		head = prev;
+
+		tail.next = reverseGroup(cur, k, numIters - 1);
+
+		return head;
+	}
+
+	private int getCount(ListNode head, int k) {
+		int count = 0;
+		while (head != null) {
+			head = head.next;
+			count++;
+		}
+		return count;
 	}
 
 	/**
