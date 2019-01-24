33fbcf4a7aa5757f8a7127c6af4d1404cb84bb66
Michael Wong <michaelzhwong@gmail.com>
2019-01-23 19:35:14 -0500

    Iterative preorder

---
+++ src/trees/Preorder.java
@@ -0,0 +1,55 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Preorder {
+	
+	/**
+	 * Iterative solution.
+	 * 
+	 * @param root - Root of the tree.
+	 * @return List<Integer> - Preorder traversal of the n-ary tree.
+	 */
+	public List<Integer> preorder(Node root) {
+		List<Integer> result = new ArrayList<Integer>();
+
+		if(root == null) {
+			return result;
+		}
+		
+		Stack<Node> stack = new Stack<Node>();
+		stack.push(root);
+		
+		while(!stack.isEmpty()) {
+			Node curNode = stack.pop();
+			result.add(curNode.val);
+			if(curNode.children != null) {
+                int size = curNode.children.size();
+				for(int i = size - 1; i >= 0; i--) {
+                    stack.push(curNode.children.get(i));
+                }
+			}
+		}
+		
+		return result;
+	}
+
+	/**
+	 * Definition for a node in an n-ary tree.
+	 * 
+	 * @author michaelwong
+	 *
+	 */
+	class Node {
+		public int val;
+		public List<Node> children;
+
+		public Node(int _val, List<Node> _children) {
+			val = _val;
+			children = _children;
+		}
+	};
+
+}
