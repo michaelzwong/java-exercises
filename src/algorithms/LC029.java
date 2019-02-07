853ee1cbe02068c80a759b47b2e0452f65ef3693
Michael Wong <michaelzhwong@gmail.com>
2019-02-07 18:22:18 -0500

    Initial commit

---
+++ src/algorithms/LC029.java
@@ -0,0 +1,27 @@
+package algorithms;
+
+/**
+ * LeetCode Medium.
+ * 
+ * @author michaelwong
+ *
+ */
+public class LC029 {
+
+	/**
+	 * Given two integers dividend and divisor, divide two integers without using
+	 * multiplication, division and mod operator.
+	 * 
+	 * Return the quotient after dividing dividend by divisor.
+	 * 
+	 * The integer division should truncate toward zero.
+	 * 
+	 * @param dividend
+	 * @param divisor
+	 * @return int - Integer division result
+	 */
+	public int divide(int dividend, int divisor) {
+		return 0;
+	}
+
+}
