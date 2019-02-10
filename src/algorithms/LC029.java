f8e80afcc7f9954a5e5d67da3e08aef85436c5a5
Michael Wong <michaelzhwong@gmail.com>
2019-02-10 16:35:27 -0500

    Efficient solution

--- src/algorithms/LC029.java
+++ src/algorithms/LC029.java
@@ -21,27 +21,55 @@ public class LC029 {
 	 * @return int - Integer division result
 	 */
 	public int divide(int dividend, int divisor) {
-		if(dividend == 0) {
-            return 0;
-        }
-        long a = Math.abs((long)dividend);
-		long b = Math.abs((long)divisor);
-        long result = 0;
-		
-		while(a - b >= 0) {
-			a -= b;
-            result++;
+		// Convert to long to accommodate overflow
+		// Use absolute values to simplify addition in calculation
+		long lDividend = Math.abs((long) dividend);
+		long lDivisor = Math.abs((long) divisor);
+
+		/* Integer division is guaranteed to be 0 */
+		// a. dividend is 0
+		// b. divisor > dividend
+		if (lDividend == 0 || lDivisor > lDividend) {
+			return 0;
 		}
-		
-		if(dividend > 0 && divisor < 0 || (dividend < 0 && divisor > 0)) {
+
+		long result = recursivelyDivide(lDividend, lDivisor);
+
+		if (dividend > 0 && divisor < 0 || (dividend < 0 && divisor > 0)) {
 			result = -result;
 		}
-		
-        if(result > Integer.MAX_VALUE) {
-            return Integer.MAX_VALUE;
-        }
-        
+
+		if (result > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE;
+		}
+
 		return (int) result;
+	}
+
+	/**
+	 * Recursively "divide" using addition.
+	 * 
+	 * 
+	 * @param dividend
+	 * @param divisor
+	 * @return long
+	 */
+	private long recursivelyDivide(long dividend, long divisor) {
+		// Base case
+		if (dividend < divisor)
+			return 0;
+
+		// Find the largest multiple such that (sum + sum) <= dividend
+		// Take increments by multiples of 1, 2, 4, 8, 16, ..., 2^n
+		long sum = divisor;
+		long multiple = 1;
+		while ((sum + sum) <= dividend) {
+			sum += sum;
+			multiple += multiple;
+		}
+
+		// Find number of multiples from the remainder (dividend - sum) recursively
+		return multiple + recursivelyDivide(dividend - sum, divisor);
 	}
 
 }
