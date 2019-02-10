3d6b6a5c82e9ef0ef9dcd4872b296fd8d86b0157
Michael Wong <michaelzhwong@gmail.com>
2019-02-09 20:56:57 -0500

    Linear runtime solution

--- src/algorithms/LC029.java
+++ src/algorithms/LC029.java
@@ -21,7 +21,27 @@ public class LC029 {
 	 * @return int - Integer division result
 	 */
 	public int divide(int dividend, int divisor) {
-		return 0;
+		if(dividend == 0) {
+            return 0;
+        }
+        long a = Math.abs((long)dividend);
+		long b = Math.abs((long)divisor);
+        long result = 0;
+		
+		while(a - b >= 0) {
+			a -= b;
+            result++;
+		}
+		
+		if(dividend > 0 && divisor < 0 || (dividend < 0 && divisor > 0)) {
+			result = -result;
+		}
+		
+        if(result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        
+		return (int) result;
 	}
 
 }
