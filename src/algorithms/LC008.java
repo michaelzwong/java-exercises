0dd3d61e0e82ab49e94fce39a0b3955ca6838633
Michael Wong <michaelzhwong@gmail.com>
2019-10-13 18:52:30 -0400

    Optimized solution

--- src/algorithms/LC008.java
+++ src/algorithms/LC008.java
@@ -29,38 +29,40 @@ public class LC008 {
 	 */
 	public int myAtoi(String str) {
 
-		int index = 0, sign = 1, total = 0;
-        // Empty string
-        if(str.length() == 0) return 0;
-
-        // Remove Spaces
-        while(index < str.length() && str.charAt(index) == ' ')
-            index ++;
-        
-        // String has no digits
-        if(index >= str.length()) {
-            return 0;
-        }
-
-        // Handle signs
-        if(str.charAt(index) == '+' || str.charAt(index) == '-'){
-            sign = str.charAt(index) == '+' ? 1 : -1;
-            index ++;
-        }
-        
-        // Convert number and avoid overflow
-        while(index < str.length()){
-            int digit = str.charAt(index) - '0';
-            if(digit < 0 || digit > 9) break;
-
-            // Check if total will be overflow after 10 times and add digit
-            if(Integer.MAX_VALUE/10 < total || Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE %10 < digit)
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-
-            total = 10 * total + digit;
-            index ++;
+        public int myAtoi(String str) {
+            // Remove all the white spaces
+            str = str.trim();
+            if(str.length() == 0) {
+                return 0;
+            }
+            int answer = 0;        
+            int sign = 1;
+            // Iterator
+            int i = 0;
+            if(str.charAt(i) == '+') {
+                i++;
+            } else if(str.charAt(i) == '-') {
+                sign = -sign;
+                i++;
+            }
+            while(i < str.length() && Character.isDigit(str.charAt(i))) {
+                int num = sign*Character.getNumericValue(str.charAt(i));
+                if(overflows(answer, num)) {
+                    return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+                answer = answer*10 + num;
+                i++;
+            }
+            return answer;
         }
         
-        return total * sign;
+        private boolean overflows(int answer, int num) {
+            return (
+                answer > Integer.MAX_VALUE/10
+                || (answer == Integer.MAX_VALUE/10 && num > Integer.MAX_VALUE % 10)
+                || answer < Integer.MIN_VALUE/10
+                || (answer == Integer.MIN_VALUE/10 && num < Integer.MIN_VALUE % 10)
+            );
+        }
 	}
 }
