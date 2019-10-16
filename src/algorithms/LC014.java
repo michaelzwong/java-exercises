3376232f108b31abd7d308f18f847a9c9298399b
Michael Wong <michaelzhwong@gmail.com>
2019-10-16 11:36:30 -0400

    Optimized

--- src/algorithms/LC014.java
+++ src/algorithms/LC014.java
@@ -15,62 +15,28 @@ public class LC014 {
 	 * @return String - Longest common prefix
 	 */
 	public String findLongestCommonPrefix(String[] strs) {
-		int arrSize = strs.length; // Length of array
-		if (strs.length == 0)
-			return "";
-		int minLength = strs[0].length(); // Shortest string in strs
-		for (int i = 0; i < arrSize; i++) { // Find the shortest string
-			if (strs[i].length() < minLength)
-				minLength = strs[i].length();
-		}
-		String testPrefix = "";
-		// Loop through the characters each string at most "minLength" times
-		for (int i = 0; i < minLength; i++) {
-			// Loop through each string in the array
-			for (int j = 0; j < arrSize; j++) {
-				// Test prefix for each string in the array
-				testPrefix = strs[0].substring(0, i + 1);
-				// System.out.println("Test Prefix: " + testPrefix);
-				// Check fo prefix matches
-				if (!testPrefix.equals(strs[j].substring(0, i + 1))) {
-					return strs[0].substring(0, i);
-				}
-			}
-		}
-		return strs[0].substring(0, minLength);
-	}
-
-	/**
-	 * 
-	 * Finds the longest common prefix string amongst an array of strings. If there
-	 * is no common prefix, return an empty string "".
-	 * 
-	 * This is a more efficient solution.
-	 * 
-	 * @param strs
-	 * @param efficient
-	 * @return
-	 */
-	public String findLongestCommonPrefix(String[] strs, boolean efficient) {
-		int arrSize = strs.length; // Length of array
-		if (strs.length == 0)
-			return "";
-		String minString = "";
-		int minLength = strs[0].length(); // Shortest string in strs
-		for (int i = 0; i < arrSize; i++) { // Find the shortest string
-			if (strs[i].length() <= minLength) {
-				minLength = strs[i].length();
-				minString = strs[i];
-			}
-		}
-		for (int i = 0; i < arrSize; i++) {
-			// If the string does not contain the minimum string
-			while (!(strs[i].substring(0, minString.length()).equals(minString))) {
-				System.out.println(minString);
-				minString = minString.substring(0, minString.length() - 1);
-			}
-
-		}
-		return minString;
+        if(strs.length == 0) {
+            return "";
+        }
+        if(strs.length == 1) {
+            return strs[0];
+        }
+        int minLength = Integer.MAX_VALUE;
+        for(int i = 0; i < strs.length; i++) {
+            minLength = Math.min(minLength, strs[i].length());
+        }
+        String answer = "";
+        int j = 0;
+        while(j < minLength) {
+            char c = strs[0].charAt(j);
+            for(int i = 1; i < strs.length; i++) {
+                if(strs[i].charAt(j) != c) {
+                    return answer;
+                }
+            }
+            answer += c;
+            j++;
+        }
+        return answer;
 	}
 }
