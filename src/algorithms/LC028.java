f16b8b2758394fb8bfb493b4bd5511663ecdd4fa
Michael Wong <michaelzhwong@gmail.com>
2019-10-25 11:14:32 -0400

    Optimized using KMP algorithm

--- src/algorithms/LC028.java
+++ src/algorithms/LC028.java
@@ -1,7 +1,7 @@
 package algorithms;
 /**
  * LeetCode Easy.
- * 
+ *
  * @author michaelwong
  *
  */
@@ -9,24 +9,62 @@ public class LC028 {
 	/**
 	 * Return the index of the first occurrence of needle in haystack, or -1 if
 	 * needle is not part of haystack.
-	 * 
+	 *
 	 * @param haystack - String to search in
 	 * @param needle   - String of interest
 	 * @return int - index of first occurrence
 	 */
-	public int strStr(String haystack, String needle) {
-		int needleLen = needle.length();
+	 public int strStr(String haystack, String needle) {
+	   // Error cases
+	   if(needle.length() == 0) {
+	       return 0;
+	   }
+	   int[] aux = getAuxArray(needle);
+	   // Counter for Haystack
+	   int i = 0;
+	   // Counter for Needle
+	   int j = 0;
+	   while(i < haystack.length() && j < needle.length()) {
+	       // The letters from haystack and needle don't match
+	       if(haystack.charAt(i) != needle.charAt(j)) {
+	           if(j == 0) {
+	               // Move to next letter in haystack
+	               i++;
+	           } else {
+	               // Use aux array to determine where to match next
+	               j = aux[j-1];
+	           }
+	       } else {
+	           i++;
+	           j++;
+	           if(j == needle.length()) {
+	               return i - j;
+	           }
+	       }
+	   }
 
-		if (needleLen == 0)
-			return 0;
-
-		for (int i = 0; i < haystack.length(); i++) {
-			if (haystack.charAt(i) == needle.charAt(0) && i + needleLen <= haystack.length()) {
-				if (haystack.substring(i, i + needleLen).equals(needle)) {
-					return i;
-				}
-			}
-		}
-		return -1;
+	   return -1;
+	}
+	// KMP algorithm
+	private int[] getAuxArray(String pattern) {
+	   int[] aux = new int[pattern.length()];
+	   int i = 0;
+	   int j = 1;
+	   while(j < pattern.length()) {
+	       // Prefix is equal to suffix
+	       if(pattern.charAt(i) == pattern.charAt(j)) {
+	           aux[j] = ++i;
+	           j++;
+	       // Prefix is not equal to suffix, check if there exists a prefix before i that is equal
+	       // to suffix
+	       } else if(pattern.charAt(i) != pattern.charAt(j) && i!=0) {
+	           i = aux[i-1];
+	       // No prefix equal to suffix possible
+	       } else {
+	           aux[j] = 0;
+	           j++;
+	       }
+	   }
+	   return aux;
 	}
 }
