8dab03b6559310f3e21cd88d2a46d71fbf4de6bf
Michael Wong <michaelzhwong@gmail.com>
2019-10-27 20:07:54 -0400

    Hashmap solution

--- src/algorithms/LC030.java
+++ src/algorithms/LC030.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 /**
  * LeetCode Hard.
- * 
+ *
  * @author michaelwong
  *
  */
@@ -17,36 +17,57 @@ public class LC030 {
 	 * same length. Find all starting indices of substring(s) in s that is a
 	 * concatenation of each word in words exactly once and without any intervening
 	 * characters.
-	 * 
+	 *
 	 * @param s - The string to search from.
 	 * @param words - The target words.
 	 * @return List<Integer> - List of starting indices of the substrings in s.
 	 */
-	public List<Integer> findSubstring(String s, String[] words) {
-		
-		// Retrieve the length of a word
-		int wordLength = words[0].length();
-		
-		// Keep track of the usage of each word
-		Map<String, Integer> wordMap = new HashMap<String, Integer>();
-		for(int i = 0; i < words.length; i ++) {
-			// If wordMap already contains the key, increment its value by 1 (number of times the word is in the HashMap)
-			if(wordMap.containsKey(words[i])) {
-				wordMap.put(words[i], wordMap.get(words[i]) + 1);
-			} else {
-				wordMap.put(words[i], 0);
-			}
-		}
-		
-		int j = 0;
-		
-		while(j + wordLength < s.length() && !wordMap.isEmpty()) {
-			String word = s.substring(j, j + wordLength);
-			if(wordMap.containsKey(word)) {
-				wordMap.remove(word);
-			}
-		}
-		return null;
-	}
+	 public List<Integer> findSubstring(String s, String[] words) {
+ 			 List<Integer> substrings = new ArrayList<Integer>();
+ 			 if(s.length()==0 || words.length==0) {
+ 					 return substrings;
+ 			 }
+ 			 int length = words.length * words[0].length();
+ 			 if(s.length() < length) {
+ 					 return substrings;
+ 			 }
+ 			 Map<String, Integer> wordMap = new HashMap<String, Integer>();
+ 			 for(String word : words) {
+ 					 if(wordMap.containsKey(word)) {
+ 							 wordMap.put(word, wordMap.get(word) + 1);
+ 					 } else {
+ 							 wordMap.put(word, 1);
+ 					 }
+ 			 }
+ 			 // Length of concatenation
+ 			 int i = 0;
+ 			 while(i < s.length() - length + 1) {
+ 					 if(isConcatenation(s.substring(i, i + length), wordMap, words[0].length(), words.length)) {
+ 							 substrings.add(i);
+ 					 }
+ 					 i++;
+ 			 }
+ 			 return substrings;
+ 	 }
+
+ 	 private boolean isConcatenation(String s, Map wordMap, int wordLength, int numWords) {
+ 			 int i = 0;
+ 			 Map<String, Integer> copy = new HashMap<String, Integer>();
+ 			 copy.putAll(wordMap);
+ 			 while(i < numWords) {
+ 					 String key = s.substring(i*wordLength, i*wordLength + wordLength);
+ 					 if(copy.containsKey(key)) {
+ 							 if(copy.get(key) > 1) {
+ 									 copy.put(key, copy.get(key) - 1);
+ 							 } else {
+ 									 copy.remove(key);
+ 							 }
+ 					 } else {
+ 							 return false;
+ 					 }
+ 					 i++;
+ 			 }
+ 			 return copy.isEmpty();
+ 	 }
 
 }
