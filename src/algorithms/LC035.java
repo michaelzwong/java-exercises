51e45e0e3535f34b31b24a6d731acb7af3be7a80
Michael Wong <michaelzhwong@gmail.com>
2019-10-30 11:11:08 -0400

    Recursive binary search

--- src/algorithms/LC035.java
+++ src/algorithms/LC035.java
@@ -1,48 +1,49 @@
 package algorithms;
 /**
  * LeetCode Easy.
- * 
+ *
  * @author michaelwong
- * 
+ *
  */
 public class LC035 {
 	/**
 	 * Given a sorted array and a target value, return the index if the target is
 	 * found. If not, return the index where it would be if it were inserted in
 	 * order. You may assume no duplicates in the array.
-	 * 
+	 *
 	 * @param nums   - array of integers
 	 * @param target - target integer
 	 * @return index
 	 */
-	public static int searchInsert(int[] nums, int target) {
-		int i = 0;
-		int len = nums.length;
-		int index = 0;
-		if (len == 0) {
-			return index;
-		}
+	 public int searchInsert(int[] nums, int target) {
+			 if(nums.length == 0) {
+					 return 0;
+			 }
+			 return recursiveBinarySearch(nums, target, 0, nums.length-1);
+	 }
 
-		while (i < nums.length) {
-			if (target == nums[i])
-				return i;
+	 private int recursiveBinarySearch(int[] nums, int target, int left, int right) {
+			 if(left == right) {
+					 if(target <= nums[left]) {
+							 return left;
+					 } else if(target > nums[left]) {
+							 return left + 1;
+					 }
+			 }
+			 if(target < nums[left]) {
+					 return left;
+			 }
+			 if(nums[right] < target) {
+					 return right + 1;
+			 }
+			 int mid = right - (right-left)/2;
 
-			if (i > 0 && i + 1 < nums.length) {
-				if (nums[i] < target && nums[i + 1] > target) {
-					return i + 1;
-				}
-			}
-
-			if (target < nums[i]) {
-				return i;
-			}
-
-			i++;
-		}
-
-		if (i == nums.length)
-			return nums.length;
-
-		return -1;
-	}
+			 if(nums[mid] == target) {
+					 return mid;
+			 } else if(nums[mid] < target) {
+					 return recursiveBinarySearch(nums, target, mid + 1, right);
+			 } else {
+					 return recursiveBinarySearch(nums, target, left, mid - 1);
+			 }
+	 }
 }
