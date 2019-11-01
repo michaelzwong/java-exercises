f411226bfd8ee98225a9a71cdaf1e5353333391e
Michael Wong <michaelzhwong@gmail.com>
2019-11-01 15:24:25 -0400

    Simplified solution

--- src/algorithms/LC036.java
+++ src/algorithms/LC036.java
@@ -21,44 +21,70 @@ public class LC036 {
 	 * @return boolean - validity.
 	 */
    public boolean isValidSudoku(char[][] board) {
-        List<Set<Character>> cols = new ArrayList<Set<Character>>();
-        List<Set<Character>> boxes = new ArrayList<Set<Character>>();
+    if(board == null || board.length != 9 || board[0].length != 9) {
+        return false;
+    }
 
-        // Rows
-        for(int i = 0; i < board.length; i++) {
-            // Create a HashSet for each row
-            Set<Character> rowSet = new HashSet<Character>();
-            // Columns
-            for(int j = 0; j < board[0].length; j++) {
-                Set<Character> colSet;
-                Set<Character> boxSet;
-                // Add a new HashSet for this column
-                if(cols.size() == j) {
-                    colSet = new HashSet<Character>();
-                    cols.add(colSet);
-                // HashSet for this column already exists
-                } else {
-                    colSet = cols.get(j);
-                }
-                if(boxes.size() == (i/3)*3 + j/3) {
-                    boxSet = new HashSet<Character>();
-                    boxes.add(boxSet);
-                } else {
-                    boxSet = boxes.get((i/3)*3 + j/3);
-                }
-                char num = board[i][j];
-                if(num != '.') {
-                    if(colSet.contains(num) || rowSet.contains(num) || boxSet.contains(num)) {
-                        return false;
-                    } else {
-                        colSet.add(num);
-                        rowSet.add(num);
-                        boxSet.add(num);
-                    }
-                }
+    // Rows
+    for(int i = 0; i < board.length; i++) {
+        Set<Character> rowSet = new HashSet<Character>();
+        Set<Character> colSet = new HashSet<Character>();
+        Set<Character> subboxSet = new HashSet<Character>();
+        // Columns
+        for(int j = 0; j < board[0].length; j++) {
+            if(board[i][j] != '.' && !rowSet.add(board[i][j])) {
+                return false;
+            }
+            if(board[j][i] != '.' && !colSet.add(board[j][i])) {
+                return false;
+            }
+            // Sub-box
+            int rowIndex = 3*(i/3) + j/3;
+            int colIndex = 3*(i%3) + j%3;
+            if(board[rowIndex][colIndex] != '.' && !subboxSet.add(board[rowIndex][colIndex])) {
+                return false;
             }
         }
-        return true;
     }
+    return true;
+//         List<Set<Character>> cols = new ArrayList<Set<Character>>();
+//         List<Set<Character>> boxes = new ArrayList<Set<Character>>();
+
+//         // Rows
+//         for(int i = 0; i < board.length; i++) {
+//             // Create a HashSet for each row
+//             Set<Character> rowSet = new HashSet<Character>();
+//             // Columns
+//             for(int j = 0; j < board[0].length; j++) {
+//                 Set<Character> colSet;
+//                 Set<Character> boxSet;
+//                 // Add a new HashSet for this column
+//                 if(cols.size() == j) {
+//                     colSet = new HashSet<Character>();
+//                     cols.add(colSet);
+//                 // HashSet for this column already exists
+//                 } else {
+//                     colSet = cols.get(j);
+//                 }
+//                 if(boxes.size() == (i/3)*3 + j/3) {
+//                     boxSet = new HashSet<Character>();
+//                     boxes.add(boxSet);
+//                 } else {
+//                     boxSet = boxes.get((i/3)*3 + j/3);
+//                 }
+//                 char num = board[i][j];
+//                 if(num != '.') {
+//                     if(colSet.contains(num) || rowSet.contains(num) || boxSet.contains(num)) {
+//                         return false;
+//                     } else {
+//                         colSet.add(num);
+//                         rowSet.add(num);
+//                         boxSet.add(num);
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+}
 
 }
