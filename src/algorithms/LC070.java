b709ef718b44b5d6d029c84a6af273417a4f4c05
Michael Wong <michaelzhwong@gmail.com>
2020-01-09 23:42:14 -0500

    Optimized solution

---
+++ src/algorithms/LC070.java
@@ -0,0 +1,35 @@
+package algorithms;
+
+/**
+ * LeetCode Medium.
+ *
+ * @author michaelwong
+ */
+public class LC064 {
+
+    /**
+     * You are climbing a stair case. It takes n steps to reach to the top.
+     *
+     * Each time you can either climb 1 or 2 steps. In how many distinct ways
+     * can you climb to the top?
+     *
+     * Note: Given n will be a positive integer.
+     *
+     * @param n - number of steps
+     * @return int - number of ways to climb to the top
+     */
+    public int climbStairs(int n) {
+       if(n <= 0) {
+           return 1;
+       }
+       int stepZero = 1;
+       int stepOne = 1;
+       int current = 1;
+       for(int i = 2; i <= n; i++) {
+           current = stepZero + stepOne;
+           stepZero = stepOne;
+           stepOne = current;
+       }
+       return current;
+    }
+}
